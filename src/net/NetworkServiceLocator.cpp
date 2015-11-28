89f53407680b552f2824b6b61aabef4d4e1f618f
David McCann <mccann.dmc@gmail.com>
2015-11-28 11:58:24 +0000

    fixed bug in NetworkServiceLocator

--- src/net/NetworkServiceLocator.cpp
+++ src/net/NetworkServiceLocator.cpp
@@ -1,8 +1,9 @@
-#include "mocca/net/NetworkServiceLocator.h"
+#include "mocca/base/StringTools.h"
 #include "mocca/net/Error.h"
 #include "mocca/net/IProtocolConnection.h"
 #include "mocca/net/IProtocolConnectionAcceptor.h"
 #include "mocca/net/LoopbackNetworkService.h"
+#include "mocca/net/NetworkServiceLocator.h"
 #include "mocca/net/NetworkServiceLocator.h"
 #include "mocca/net/TCPNetworkAddress.h"
 #include "mocca/net/TCPNetworkService.h"
@@ -21,25 +22,26 @@ std::shared_ptr<IProtocolNetworkService> NetworkServiceLocator::service(const st
 }
 
 void NetworkServiceLocator::provideService(std::shared_ptr<IProtocolNetworkService> service) {
-    for (auto& service : services_) {
-        if (service->transport() == service->transport()) {
-            throw NetworkError("Network service for transport '" + service->transport() +
-                                   "' already provided to locator", __FILE__, __LINE__);
+    for (auto& s : services_) {
+        if (s->protocol() == service->protocol() && s->transport() == service->transport()) {
+            throw NetworkError(mocca::formatString("Network service for protocol '%%' and transport '%%' already provided to locator",
+                                                   service->protocol(), service->transport()),
+                               __FILE__, __LINE__);
         }
     }
     services_.push_back(service);
 }
 
-void mocca::net::NetworkServiceLocator::removeAll() { services_.clear(); }
+void mocca::net::NetworkServiceLocator::removeAll() {
+    services_.clear();
+}
 
-std::unique_ptr<IProtocolConnectionAcceptor>
-mocca::net::NetworkServiceLocator::bind(const Endpoint& endpoint) {
+std::unique_ptr<IProtocolConnectionAcceptor> mocca::net::NetworkServiceLocator::bind(const Endpoint& endpoint) {
     auto serv = service(endpoint.protocol(), endpoint.transport());
     return serv->bind(endpoint.connectionString());
 }
 
-std::unique_ptr<IProtocolConnection>
-mocca::net::NetworkServiceLocator::connect(const Endpoint& endpoint) {
+std::unique_ptr<IProtocolConnection> mocca::net::NetworkServiceLocator::connect(const Endpoint& endpoint) {
     auto serv = service(endpoint.protocol(), endpoint.transport());
     return serv->connect(endpoint.connectionString());
 }
