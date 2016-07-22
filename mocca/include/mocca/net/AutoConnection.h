c4baf37bd979e737ed58e5d19abb4737cab0ea56
David McCann <mccann.dmc@gmail.com>
2016-07-22 19:09:55 +0200

    added retry mechanism for connections

--- mocca/include/mocca/net/AutoConnection.h
+++ mocca/include/mocca/net/AutoConnection.h
@@ -1,8 +1,13 @@
 #pragma once
 
+#include "mocca/log/LogManager.h"
+#include "mocca/net/ConnectionFactorySelector.h"
 #include "mocca/net/IMessageConnection.h"
+#include "mocca/net/NetworkError.h"
 
+#include <chrono>
 #include <memory>
+#include <thread>
 
 namespace mocca {
 namespace net {
@@ -14,6 +19,25 @@ public:
     std::shared_ptr<const ConnectionID> connectionID() const;
     void send(Message message) const;
     Message receive() const;
+
+private:
+    template <typename T> typename std::result_of<T()>::type retry(const T& fun) const {
+        const int maxRetries = 10;
+        auto waitTime = std::chrono::seconds(10);
+        int retries = 0;
+        while (true) {
+            try {
+                return fun();
+            } catch (const mocca::net::NetworkError&) {
+                ++retries;
+                LWARNING("Failed to connect to " << m_endpoint << " (retry " << retries << ")");
+                if (retries == maxRetries) {
+                    throw;
+                }
+                std::this_thread::sleep_for(waitTime);
+            }
+        }
+    }
 
 private:
     Endpoint m_endpoint;
--- mocca/src/net/AutoConnection.cpp
+++ mocca/src/net/AutoConnection.cpp
@@ -1,7 +1,5 @@
 #include "mocca/net/AutoConnection.h"
 
-#include "mocca/net/ConnectionFactorySelector.h"
-
 using namespace mocca::net;
 
 AutoConnection::AutoConnection(const Endpoint& endpoint)
@@ -9,22 +7,29 @@ AutoConnection::AutoConnection(const Endpoint& endpoint)
     , m_connection(nullptr) {}
 
 std::shared_ptr<const ConnectionID> AutoConnection::connectionID() const {
-    if (m_connection == nullptr || !m_connection->isConnected()) {
-        m_connection = ConnectionFactorySelector::connect(m_endpoint);
-    }
-    return m_connection->connectionID();
+    return retry([&] {
+        if (m_connection == nullptr || !m_connection->isConnected()) {
+            m_connection = ConnectionFactorySelector::connect(m_endpoint);
+        }
+        return m_connection->connectionID();
+    });
 }
 
-void mocca::net::AutoConnection::send(Message message) const {
-    if (m_connection == nullptr || !m_connection->isConnected()) {
-        m_connection = ConnectionFactorySelector::connect(m_endpoint);
-    }
-    m_connection->send(std::move(message));
+void AutoConnection::send(Message message) const {
+    retry([&] {
+        if (m_connection == nullptr || !m_connection->isConnected()) {
+            m_connection = ConnectionFactorySelector::connect(m_endpoint);
+        }
+        m_connection->send(message);
+        return 0;
+    });
 }
 
-mocca::net::Message mocca::net::AutoConnection::receive() const {
-    if (m_connection == nullptr || !m_connection->isConnected()) {
-        m_connection = ConnectionFactorySelector::connect(m_endpoint);
-    }
-    return m_connection->receive();
+Message AutoConnection::receive() const {
+    return retry([&] {
+        if (m_connection == nullptr || !m_connection->isConnected()) {
+            m_connection = ConnectionFactorySelector::connect(m_endpoint);
+        }
+        return m_connection->receive();
+    });
 }
